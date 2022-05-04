a792904bc7e40231f95405d2da6c814fb8894a59
Fauzi Alhari <fauzialhari@gmail.com>
2022-05-04 16:20:41 +0700

    Initialize root triangle on canvas

---
+++ main.js
@@ -0,0 +1,54 @@
+"use strict";
+class SierpinskiTriangle {
+  mainCanvas = document.getElementById("sierpinski-triangle").getContext("2d");
+  rootTriangle = new Path2D();
+
+  get canvasHeight() {
+    console.log(this.mainCanvas);
+    return this.mainCanvas.canvas.height;
+  }
+
+  get canvasWidth() {
+    return this.mainCanvas.canvas.width;
+  }
+
+  constructor() {
+    this.initializeFirstThreeDots();
+  }
+
+  initializeFirstThreeDots() {
+    // Create an equilateral triangle in the middle of canvas
+    // https://stackoverflow.com/a/8937325
+    const centerPointOfCanvas = this.canvasWidth / 2;
+    const middlePointOfCanvas = this.canvasHeight / 2;
+    const THREE_EVEN_RADIAN_ANGLES = [
+      Math.PI / 6,
+      (3 * Math.PI) / 2,
+      (5 * Math.PI) / 6,
+    ];
+    const radiusOfMaximumCircleInCanvas =
+      this.canvasWidth < this.canvasHeight
+        ? centerPointOfCanvas
+        : middlePointOfCanvas;
+    THREE_EVEN_RADIAN_ANGLES.forEach((radianAngle, index) => {
+      const x =
+        radiusOfMaximumCircleInCanvas * Math.cos(radianAngle) +
+        centerPointOfCanvas;
+      const y =
+        radiusOfMaximumCircleInCanvas * Math.sin(radianAngle) +
+        middlePointOfCanvas;
+      if (index > 0) {
+        // if not the first iteration
+        this.rootTriangle.lineTo(x, y);
+      } else {
+        this.rootTriangle.moveTo(x, y);
+      }
+    });
+    // draw line back to first point
+    this.rootTriangle.closePath();
+    this.mainCanvas.fill(this.rootTriangle);
+  }
+
+}
+
+new SierpinskiTriangle();
